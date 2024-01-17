d4384b40ff98e382b32c7a2900bd98fc2a9f6f3e
Tamara Montijo <tamaramontijo@wizsolucoes.com.br>
2024-01-17 20:13:29 -0300

    test: e2e

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -1,7 +1,6 @@
 describe('search products', () => {
   it('should be able to search for products', () => {
-    cy.visit('/')
-    cy.get('input[name=q]').type('moletom').parent('form').submit()
+    cy.searchByQuery('moletom')
 
     cy.location('pathname').should('include', '/search')
     cy.location('search').should('include', 'q=moletom')
@@ -12,8 +11,8 @@ describe('search products', () => {
     cy.on('uncaught:exception', () => {
       return false
     })
-    cy.visit('/search')
 
+    cy.visit('/search')
     cy.location('pathname').should('equal', '/')
   })
 })
