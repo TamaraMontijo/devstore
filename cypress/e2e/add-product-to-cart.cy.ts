803f63dbcee4196781487f72272dbe98dfc57dd7
Tamara Montijo <tamaramontijo@wizsolucoes.com.br>
2024-01-18 10:53:10 -0300

    fix: env file

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -3,6 +3,7 @@ describe('add product to cart', () => {
     cy.visit('/')
   })
   it('should be able to navigate to the product page and add it to the cart', () => {
+    cy.searchByQuery('moletom')
     cy.get('a[href^="/product"]').first().click()
 
     cy.location('pathname').should('include', '/product')
